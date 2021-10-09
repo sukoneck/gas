886c9786941a059762f161cfce9e99cf6351719a
sukoneck <19413126+sukoneck@users.noreply.github.com>
2021-10-08 17:44:38 -0700

    Update lgtm.sh

--- lgtm.sh
+++ lgtm.sh
@@ -1,1 +1,8 @@
-function gas { echo $(($(echo $(echo $(($(curl https://cloudflare-eth.com -s -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":"'$RANDOM'"}' | jq ."result" -r)))))/1000000000)); }
+# set function as var
+func='function gas { echo $(($(echo $(echo $(($(curl https://cloudflare-eth.com -s -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":"'$RANDOM'"}' | jq ."result" -r)))))/1000000000)); }'
+
+# slap it in 
+sudo echo $func >> .bashrc
+
+# use it
+gas
